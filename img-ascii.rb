1c2060789dd13639b3df8c916bfecb7f893d7e33
Devin Riley <devin@Macintosh.local>
2014-04-03 10:42:26 -0700

    Use rmagick api to iterate pixels

--- img-ascii.rb
+++ img-ascii.rb
@@ -4,30 +4,33 @@ require 'rmagick'
 require 'debugger'
 include Magick
 
-ASCII_HEIGHT = 50 
+WIDTH = 150 
 ascii = []
 
-scene = ImageList.new('text.jpg')
-aspect_ratio = scene.rows.to_f / scene.columns.to_f
-sub_sampled = scene.sample(ASCII_HEIGHT, aspect_ratio * ASCII_HEIGHT)
-grayscale = sub_sampled.quantize(256, Magick::GRAYColorspace)
-grayscale.export_pixels.each_slice(3) do |pixels|
-  slice_sum = pixels.inject{|sum, p| sum + p}.to_f #sum color channels
-  scaled_pixel = slice_sum / (MaxRGB * 3) # scale between 0 and 1
+image = ImageList.new('funny.jpg')
+image = image.scale(WIDTH / image.columns.to_f)
+image = image.scale(image.columns, image.rows / 2.3)
+grayscale = image.quantize(256, Magick::GRAYColorspace)
+grayscale.each_pixel do |pixel|
+  scaled_pixel = pixel.intensity.to_f / MaxRGB
 
   if scaled_pixel < 0.2 
-    3.times {ascii << ' '}
+    ascii << ' '
   elsif scaled_pixel >= 0.2 && scaled_pixel < 0.4
-    3.times {ascii << '.'}
+    ascii << '.'
   elsif scaled_pixel >= 0.4 && scaled_pixel < 0.6
-    3.times { ascii << '░' }
+     ascii << 'i'
   elsif scaled_pixel >= 0.6 && scaled_pixel < 0.8
-    3.times {ascii << '▒'}
+    ascii << '#'
   else
-    3.times { ascii << '◙'}
+     ascii << '◙'
   end
 end
-ascii.each_with_index { |char, index| print char; print "\n" if index % (3 * ASCII_HEIGHT) == 0 }
+
+ascii.each_with_index do |char, index| 
+  print char
+  print "\n" if index % WIDTH == 0
+end
 exit
 
 

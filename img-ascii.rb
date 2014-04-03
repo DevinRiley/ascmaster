09e53f47d7209e8d33131e578486b04bd8de3908
Devin Riley <devin@Macintosh.local>
2014-04-03 11:14:37 -0700

    Add converter class

--- img-ascii.rb
+++ img-ascii.rb
@@ -2,35 +2,54 @@
 
 require 'rmagick'
 require 'debugger'
+
 include Magick
 
-WIDTH = 150 
-ascii = []
+class Converter
 
-image = ImageList.new('funny.jpg')
-image = image.scale(WIDTH / image.columns.to_f)
-image = image.scale(image.columns, image.rows / 2.3)
-grayscale = image.quantize(256, Magick::GRAYColorspace)
-grayscale.each_pixel do |pixel|
-  scaled_pixel = pixel.intensity.to_f / MaxRGB
+  def initialize(width)
+    @width = width
+  end
 
-  if scaled_pixel < 0.2 
-    ascii << ' '
-  elsif scaled_pixel >= 0.2 && scaled_pixel < 0.4
-    ascii << '.'
-  elsif scaled_pixel >= 0.4 && scaled_pixel < 0.6
-     ascii << 'i'
-  elsif scaled_pixel >= 0.6 && scaled_pixel < 0.8
-    ascii << '#'
-  else
-     ascii << '◙'
+  def image_to_ascii(image)
+    ascii = []
+
+    image.each_pixel do |pixel|
+      scaled_pixel = pixel.intensity.to_f / MaxRGB
+
+      if scaled_pixel < 0.2 
+        ascii << ' '
+      elsif scaled_pixel >= 0.2 && scaled_pixel < 0.4
+        ascii << '.'
+      elsif scaled_pixel >= 0.4 && scaled_pixel < 0.6
+         ascii << 'i'
+      elsif scaled_pixel >= 0.6 && scaled_pixel < 0.8
+        ascii << '#'
+      else
+         ascii << '◙'
+      end
+    end
+
+    return ascii
+  end
+
+  def convert(image_name)
+    image = ImageList.new(image_name)
+    image = image.scale(@width / image.columns.to_f)
+    image = image.scale(image.columns, image.rows / 2.3)
+
+    grayscale = image.quantize(256, Magick::GRAYColorspace)
+    
+    image_to_ascii(grayscale).each_with_index do |char, index| 
+      print char
+      print "\n" if index % @width == 0
+    end
+
   end
 end
 
-ascii.each_with_index do |char, index| 
-  print char
-  print "\n" if index % WIDTH == 0
-end
+# From command line
+Converter.new(70).convert('funny.jpg')
 exit
 
 
